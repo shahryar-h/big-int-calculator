b2254550c8f6d1f9f152756b0656ad831c4aa03b
Shahryar haghighifard <shahryar@Shahryars-MacBook-Pro.local>
2020-11-24 12:27:10 -0500

    states updated and works well

--- src/components/pieChart/calculator.component.jsx
+++ src/components/pieChart/calculator.component.jsx
@@ -2,78 +2,170 @@ import React from 'react';
 // import ReactDOM from 'react-dom';
 
 
-
-
 class Calculator extends React.Component {
   constructor(){
     super()
     this.validateInt = this.validateInt.bind(this);
     this.numberClicked = this.numberClicked.bind(this);
-    this.addition = this.addition.bind(this);
-    this.subtraction = this.subtraction.bind(this);
     // this.addition = this.addition.bind(this);
-    // this.addition = this.addition.bind(this);
+    // this.subtraction = this.subtraction.bind(this);
+    // this.multiplication = this.multiplication.bind(this);
+    // this.division = this.division.bind(this);
     this.validateOutputBox = this.validateOutputBox.bind(this);
-    this.calc = this.calc.bind(this);
-    
-  //   this.state = {
-  //     equation: "0",
-  //     currentOpertion: ""
-  
-  // };
-  this.equation = "";
-  this.currentOpertion = "";
+    this.generalblabal = this.generalblabal.bind(this);
+    this.addition = this.generalblabal.bind(this,"+");
+    this.subtraction = this.generalblabal.bind(this,"-");
+    this.multiplication = this.generalblabal.bind(this,"*");
+    this.division = this.generalblabal.bind(this,"/");
+    this.calc = this.calc.bind(this); 
+    this.state = {
+
+      equation: "",
+      operand: "",
+      currentOpertion: "",
+      error: undefined,
+
+  };
+  // this.equation = "";
+  // this.currentOpertion = "";
   }
   
+  
+
+  componentDidMount(){
+    console.log(this.state);
+  }
+
+  componentDidUpdate(){
+    console.log(this.state);
+  }
+
   // adds digits to outputBox as clicked
   numberClicked(number) {
-    let outputBox = document.getElementById('outputBox');
-    outputBox.value = outputBox.value + number;
+    const {equation,operand,currentOpertion,error } = this.state;
+
+    this.setState(({operand}) => ({operand: operand + number}));
   }
 
   
 // validate outputBox for correct input prior to updating the state
-  validateOutputBox(outputBox){
-    let currentInput = outputBox.value;
+  validateOutputBox(){
+    const {equation,operand,currentOpertion,error } = this.state;
+
+    let currentInput = operand;
       if(currentInput === ""  ) {
-      outputBox.placeholder = "I need an Integer here! not an empty input";
+      this.setState({error: "I need an Integer here! not an empty input"});
     }else{
       let isInt = this.validateInt(currentInput);
       if(isInt === false){
-        outputBox.value = "";
-        outputBox.placeholder = "This calculator is for Integers Only!";
+        this.setState({
+          operand: "",
+          error: "I need an Integer here! not an empty input"
+
+      
+      });
+
       }else{return true}
     }
   }
 
-  addition(){
-    let outputBox = document.getElementById('outputBox');
-    let isInt = this.validateOutputBox(outputBox);
+  generalblabal(operator){
+    const {equation,operand,currentOpertion,error } = this.state;
+
+    let isInt = this.validateOutputBox();
     if (isInt === true){
-      // this.setState((state) => ({ equation: state.equation + "+" + outputBox.value }));
-      // console.log(this.he);
-      this.equation = this.equation + "+" + outputBox.value;
+
+      if (equation === "") {
+        this.setState(({equation}) => ({ equation: equation + operand }));
+      }else{
+        this.setState(({equation}) => ({ equation: equation + operator + operand }));
+      }
 
     }
-    this.currentOpertion = "+";
-    outputBox.value = "";
+
+    this.setState(({currentOpertion}) => ({ 
+      currentOpertion: operator,
+      operand: ""
+    }));
+    
   }
-  subtraction(){
-    let outputBox = document.getElementById('outputBox');
-    let isInt = this.validateOutputBox(outputBox);
-    if (isInt === true){
-      // this.setState((state) => ({ equation: state.equation + "+" + outputBox.value }));
-      // console.log(this.he);
-      if (this.equation === "") {
-        this.equation = this.equation + outputBox.value;
-      }else{
-        this.equation = this.equation + "-" + outputBox.value;
-      }
-    }
-    this.currentOpertion = "-";
-    outputBox.value = "";
-    console.log(this.equation);
-  }
+
+  // addition(){
+  //   const {equation,operand,currentOpertion,error } = this.state;
+
+  //   let isInt = this.validateOutputBox();
+  //   if (isInt === true){
+
+  //     if (equation === "") {
+  //       this.setState(({equation}) => ({ equation: equation + operand }));
+  //     }else{
+  //       this.setState(({equation}) => ({ equation: equation + "+" + operand }));
+  //     }
+
+  //   }
+
+  //   this.setState(({currentOpertion}) => ({ 
+  //     currentOpertion: "+",
+  //     operand: ""
+  //   }));
+    
+  // }
+
+
+  // division(){
+  //   let outputBox = document.getElementById('outputBox');
+  //   let isInt = this.validateOutputBox(outputBox);
+  //   if (isInt === true){
+  //     // this.setState((state) => ({ equation: state.equation + "+" + outputBox.value }));
+  //     // console.log(this.he);
+  //     if (this.equation === "") {
+  //       this.equation = this.equation + outputBox.value;
+  //     }else{
+  //       this.equation = this.equation + "/" + outputBox.value;
+  //     }
+
+  //   }
+  //   this.currentOpertion = "/";
+  //   outputBox.value = "";
+  // }
+//   subtraction(){
+//     const {equation,operand,currentOpertion,error } = this.state;
+
+//     let isInt = this.validateOutputBox();
+//     if (isInt === true){
+
+//       if (equation === "") {
+//         this.setState(({equation}) => ({ equation: equation + operand }));
+//       }else{
+//         this.setState(({equation}) => ({ equation: equation + "-" + operand }));
+//       }
+
+//     }
+
+//     this.setState(({currentOpertion}) => ({ 
+//       currentOpertion: "-",
+//       operand: ""
+//     }));
+    
+  
+//  }
+
+  // multiplication(){
+  //   let outputBox = document.getElementById('outputBox');
+  //   let isInt = this.validateOutputBox(outputBox);
+  //   if (isInt === true){
+  //     // this.setState((state) => ({ equation: state.equation + "+" + outputBox.value }));
+  //     // console.log(this.he);
+  //     if (this.equation === "") {
+  //       this.equation = this.equation + outputBox.value;
+  //     }else{
+  //       this.equation = this.equation + "*" + outputBox.value;
+  //     }
+
+  //   }
+  //   this.currentOpertion = "*";
+  //   outputBox.value = "";
+  // }
   // a helper function to validate if the input is an Integer
   // returns true if the input is integer
   validateInt(currentInput) {
@@ -90,48 +182,58 @@ class Calculator extends React.Component {
   }
   // start by implementing this method. justt give state to this method and use the logic we worked on already
   calc(){
-    let outputBox = document.getElementById('outputBox');
-    let isInt = this.validateOutputBox(outputBox);
-    if (isInt === true){
-      this.equation = this.equation + this.currentOpertion + outputBox.value;
-    }
-    outputBox.value = "";
-    var o = this.equation.split(/(\/|\+|\-|\*)/);
-    for(var i=0 ; i < o.length; i++ ){
+    const {equation,operand,currentOpertion,error } = this.state;
+
+    // let isInt = this.validateOutputBox();
+    // if (isInt === true){
+
+    //   if (equation === "") {
+    //     this.setState(({equation}) => ({ equation: equation + operand }));
+    //   }else{
+        
+    //   }
+
+    // }
+    // console.log(`${equation}${currentOpertion}${operand}`);
+    // this.setState(({equation}) => ({ equation: equation + `${currentOpertion}${operand}` }));
     
-      if (["/","*"].includes(o[i])){
-          
+    this.setState(() => ({ 
+      currentOpertion: "",
+      operand: ""
+    }));
+
+    console.log(equation);
+    var o = `${equation}${currentOpertion}${operand}`.split(/(\/|\+|\-|\*)/);
+
+    for(var i = 0 ; i < o.length; i++ ){
+    
+      if (["*","/"].includes(o[i])){
           switch(o[i]) {
               case "/":
-                  o[i-1] = o[i-1] / o[i+1];
-                  o.splice(i, 2) 
+                  o[i-1] = parseInt(o[i-1]) / parseInt(o[i+1]);
+                  o.splice(i, 2); 
                   i--;
-                  console.log(o);
                   break;
               case "*":
-                  o[i-1] = o[i-1] * o[i+1];
+                  o[i-1] = parseInt(o[i-1]) * parseInt(o[i+1]);
                   o.splice(i, 2) 
                   i--;
-                  console.log(o);
                   break;
           }
     
       }
-      
-    
+       
   }
     for(var i = 0 ; i < o.length; i++ ){
     
       if (["+","-"].includes(o[i])){
           switch(o[i]) {
               case "-":
-                 
-                  o[i-1] = o[i-1] - o[i+1];
-                  o.splice(i, 2) 
+                  o[i-1] = parseInt(o[i-1]) - parseInt(o[i+1]);
+                  o.splice(i, 2); 
                   i--;
                   break;
               case "+":
-                  
                   o[i-1] = parseInt(o[i-1]) + parseInt(o[i+1]);
                   o.splice(i, 2) 
                   i--;
@@ -142,28 +244,30 @@ class Calculator extends React.Component {
       
     
   }
-    this.equation = "0";
-    console.log(o);
+  this.setState(() => ({ 
+    operand: o[0]
+  }));
 
+  this.setState(() => ({ 
+    equation: ""
+  }));
   }
   
   render() {
+    const {equation,operand,currentOpertion,error } = this.state;
 
-    
     return (
       <div className="mainContent">
         <div className="calculator">
-
 
         <div className="operators operators1">
         <div onClick={this.calc} className="operator" id='equals'><h1>=</h1></div>
         <div className="operator" id='clear'><h1>C</h1></div>
 
-
-
         </div>
         <div className="dialPad">
         <div className="numbers">
+
           <div onClick={() => this.numberClicked(1)} className="rect"><h2 id='num1'>1</h2></div>
           <div onClick={() => this.numberClicked(2)} className="rect"><h2 id='num2'>2</h2></div>
           <div onClick={() => this.numberClicked(3)} className="rect"><h2 id='num3'>3</h2></div>
@@ -184,32 +288,29 @@ class Calculator extends React.Component {
         <div className="numbers">
           <div onClick={() => this.numberClicked(0)} className="rect"><h2 id='num10'>0</h2></div>
           
-          
-
         </div>
-
- 
 
         </div>
         <div className="operators operators2">
 
           <div onClick={this.addition} className="operator" id='additioin'><h1>+</h1></div>
-          <div onClick={this.subtraction}className="operator" id='subtraction'><h1>-</h1></div>
-          <div className="operator" id='multiplication'><h1>*</h1></div>
-          <div className="operator" id='division'><h1>/</h1></div>
+          <div onClick={this.subtraction} className="operator" id='subtraction'><h1>-</h1></div>
+          <div onClick={this.multiplication} className="operator" id='multiplication'><h1>*</h1></div>
+          <div onClick={this.division} className="operator" id='division'><h1>/</h1></div>
 
         </div>
-        
 
         </div>   
         
         <form>
-          <input className="output" id='outputBox' type="text" />
+
+          <input className="output" id='outputBox' type="text" placeholder={error ? error : ""} value={this.state.operand} onChange={({target:{value}}) => this.setState({operand: value})}/>
+
         </form>
       
       </div>
+
     );
   }
 }
-
-export default Calculator
+export default Calculator;
