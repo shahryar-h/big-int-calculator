68de4c9027aae676ac65163b9843873a8418e847
Shahryar haghighifard <shahryar@Shahryars-MacBook-Pro.local>
2020-11-26 13:39:37 -0500

    refactored addition and subtraction methods

--- src/components/pieChart/calcLogic.js
+++ src/components/pieChart/calcLogic.js
@@ -9,7 +9,90 @@ function swap(num1,num2) {
     num1 = num2;
     num2 = temp;
 }
-var i = "1002-3";
+
+function replaceAtIndex(number, browed, index) {
+    let len = number.length;
+    let firstHalf = number.substring(0,index);
+    let secondHalf = number.substring(index+1);
+    return firstHalf + browed + secondHalf;
+}
+
+function bigIntSubtraction(num1, num2) {
+    if(num1.localeCompare(num2, undefined, {numeric: true}) == -1) {
+        swap(num1,num2);
+    }
+    let brrow = 0;
+    let res = [];
+
+    while (num1 != 0 && num2 != 0)
+        {
+            dig1 = num1[num1.length-1];
+            dig2 = num2[num2.length-1];
+            if (dig2 > dig1) {
+                dig1 = "1" + dig1;
+                let index = 2;
+                while (num1[num1.length-index] == "0") {
+                    index++;
+                }
+                if (num1[(num1.length)-index] !== "0") {
+                    browed =  num1[(num1.length)-index];
+                    browed -= "1";
+                    q= num1.substring(0, num1.length-index);
+                    q = q + browed;
+                    w = num1.substring(num1.length-(index-1));
+                    let index2 = 0;
+                    while (w[index2] == "0") {
+                        w = replaceAtIndex(w,9,index2);
+                        index2++;  
+                    }
+                    num1 = q+w;                
+                }
+            }
+            num1 = String(Math.floor(num1/10));
+            num2 = String(Math.floor(num2/10));
+            sub = dig1-dig2;
+            res.unshift(sub);
+
+        }
+        if (num1 != 0) {
+            res.unshift(num1);
+        }
+      let res2 = res.join("");
+      res3 = parseInt(res2);
+      return String(res3);
+    
+}
+
+function bigIntAddition(num1,num2) {
+        let carry = 0;
+        res = [];
+        while (num1 != 0 && num2 != 0)
+        {
+            dig1 = num1%10;
+            dig2 = num2%10;
+            num1 = Math.floor(num1/10);
+            num2 = Math.floor(num2/10);
+            sum = dig1+dig2+carry;
+            finalDigit = sum%10;
+            carry = Math.floor(sum/10);
+            res.unshift(finalDigit);
+
+        }
+        if (num1 == 0)
+        {
+            carry+=num2;
+            res.unshift(carry);
+        }else if (num2 == 0)
+        {
+            carry+=num1;
+            res.unshift(carry);
+        }
+        return res.join("");
+    
+}
+
+
+var i = "12-3+4";
 
 
 var o = i.split(/(\/|\+|\-|\*)/);
@@ -20,90 +103,18 @@ var o = i.split(/(\/|\+|\-|\*)/);
           switch(o[i]) {
               case "-":
                 num1 = o[i-1];
-                num2 = o[i+1];  
-                if(num1.localeCompare(num2, undefined, {numeric: true}) == -1) {
-                    swap(num1,num2);
-                }
-                let brrow = 0;
-                let res = [];
-
-                while (num1 != 0 && num2 != 0)
-                    {
-                        dig1 = num1[num1.length-1];
-                        dig2 = num2[num2.length-1];
-                        if (dig2 > dig1) {
-                            dig1 = "1" + dig1;
-                            let index = 2;
-                            while (num1[num1.length-index] == "0") {
-                                index++;
-                            }
-                            if (num1[(num1.length)-index] !== "0") {
-                                browed =  num1[(num1.length)-index];
-                                browed -= "1";
-                                q= num1.substring(0, num1.length-index);
-                                q = q + browed;
-                                w = num1.substring(num1.length-(index-1));
-                                let index2 = 0;
-                                while (w[index2] == "0") {
-                                    firstPart = w.substring(0,index2);
-                                    firstPart += 9;
-                                    secondPart = w.substring(index2+1);
-                                    w = firstPart+secondPart;
-                                    index2++;  
-                                }
-                                num1 = q+w;                
-                            }
-                        }
-                        num1 = String(Math.floor(num1/10));
-                        num2 = String(Math.floor(num2/10));
-                        sub = dig1-dig2;
-                        res.unshift(sub);
-
-                    }
-                    if (num1 != 0) {
-                        res.unshift(num1);
-                    }
-
-
-                  let res2 = res.join("");
-                  res3 = parseInt(res2); 
-                  o[i-1] = String(res3);
-                  o.splice(i, 2); 
-                  i--;
-                  break;
+                num2 = o[i+1];
+                o[i-1] = bigIntSubtraction(num1, num2);  
+                o.splice(i, 2); 
+                i--;
+                break;
               case "+":
-                  num1 = parseInt(o[i-1]);
-                  num2 = parseInt(o[i+1]);
-                  let carry = 0;
-                  res = [];
-                  while (num1 != 0 && num2 != 0)
-                    {
-                        dig1 = num1%10;
-                        dig2 = num2%10;
-                        num1 = Math.floor(num1/10);
-                        num2 = Math.floor(num2/10);
-                        sum = dig1+dig2+carry;
-                        finalDigit = sum%10;
-                        carry = Math.floor(sum/10);
-                        res.unshift(finalDigit);
-
-                    }
-                    if (num1 == 0)
-                    {
-                        carry+=num2;
-                        res.unshift(carry);
-                    }else if (num2 == 0)
-                    {
-                        carry+=num1;
-                        res.unshift(carry);
-                    }
-                    o[i-1] = res.join("");
-                
-                  
-                  
-                  o.splice(i, 2) 
-                  i--;
-                  break;
+                num1 = parseInt(o[i-1]);
+                num2 = parseInt(o[i+1]);
+                o[i-1] = bigIntAddition(num1,num2);
+                o.splice(i, 2) 
+                i--;
+                break;
           }
     
       }
@@ -111,7 +122,7 @@ var o = i.split(/(\/|\+|\-|\*)/);
     
   }
 
-// console.log(o);
+console.log(o);
 
 
 // num1 = parseInt(o[i-1]);
