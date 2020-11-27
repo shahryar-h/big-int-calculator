dff634d39429e6c68108a61a041798b8f0b5b8c7
Shahryar haghighifard <shahryar@Shahryars-MacBook-Pro.local>
2020-11-27 12:45:21 -0500

    major debugging of add functions

--- src/components/pieChart/calcLogic.js
+++ src/components/pieChart/calcLogic.js
@@ -48,8 +48,8 @@ function bigIntSubtraction(num1, num2) {
                     num1 = q+w;                
                 }
             }
-            num1 = String(Math.floor(num1/10));
-            num2 = String(Math.floor(num2/10));
+            num1 = num1.substring(0,num1.length-1);
+            num2 = num2.substring(0,num2.length-1);
             sub = dig1-dig2;
             res.unshift(sub);
 
@@ -58,8 +58,8 @@ function bigIntSubtraction(num1, num2) {
             res.unshift(num1);
         }
       let res2 = res.join("");
-      res3 = parseInt(res2);
-      return String(res3);
+      
+      return String(res2);
     
 }
 
@@ -68,10 +68,10 @@ function bigIntAddition(num1,num2) {
         res = [];
         while (num1 != 0 && num2 != 0)
         {
-            dig1 = num1%10;
-            dig2 = num2%10;
-            num1 = Math.floor(num1/10);
-            num2 = Math.floor(num2/10);
+            dig1 = parseInt(num1.substring(num1.length-1));
+            dig2 = parseInt(num2.substring(num2.length-1));
+            num1 = num1.substring(0,num1.length-1);
+            num2 = num2.substring(0,num2.length-1);
             sum = dig1+dig2+carry;
             finalDigit = sum%10;
             carry = Math.floor(sum/10);
@@ -84,10 +84,23 @@ function bigIntAddition(num1,num2) {
             res.unshift(carry);
         }else if (num2 == 0)
         {
-            carry+=num1;
-            res.unshift(carry);
+            while (num1 != 0 ) {
+                dig1 = parseInt(num1.substring(num1.length-1));
+                num1 = num1.substring(0,num1.length-1);
+                sum = dig1+carry;
+                finalDigit = sum%10;
+                carry = Math.floor(sum/10);
+                res.unshift(finalDigit);
+            }
+            if (carry != 0 ) {
+                res.unshift(carry);
+            }
+
+            
         }
-        return res.join("");
+        resultt = res.join("");
+        resultt = resultt.replace(/^0+/, '');
+        return resultt;
     
 }
 
@@ -105,8 +118,8 @@ function bigIntMultiplication(num1, num2) {
     carry = 0;
     forAddition = [];
         for(let j = 0 ; j < num2length ; j++) {
-        multiplier = parseInt(num2 % 10);
-        num2 = String(Math.floor(num2/10));
+        multiplier = parseInt(num2.substring(num2.length-1));
+        num2 = num2.substring(0,num2.length-1);
         for(let k = 0; k< j ; k++){
             numToAdd.unshift("0");
         }
@@ -121,6 +134,7 @@ function bigIntMultiplication(num1, num2) {
                 numToAdd.unshift(lastDigit); 
             }else{
                 numToAdd.unshift(digits);
+                carry = "0";
             }            
         }
         forAddition.unshift(numToAdd.join(""))
@@ -131,68 +145,67 @@ function bigIntMultiplication(num1, num2) {
     }
 
     while (forAddition.length > 1) {
-        forAddition[1] = bigIntAddition(forAddition[0],forAddition[1]);
-        forAddition.splice(0,1);
+        ba = bigIntAddition(forAddition[0],forAddition[1])
+        forAddition[0] = ba;
+        forAddition.splice(1,1);
     }
     
-    return String(parseInt(forAddition[0]));
+    return forAddition[0];
     
     
 
 }
 
 
-var i = "1555*1444+2-1";
-var o = i.split(/(\/|\+|\-|\*)/);
+// var i = "1555*1444+2-1";
+// var o = i.split(/(\/|\+|\-|\*)/);
 
 
-for(var i = 0 ; i < o.length; i++ ){
+// for(var i = 0 ; i < o.length; i++ ){
     
-    if (["*","/"].includes(o[i])){
-        switch(o[i]) {
-            case "/":
-                o[i-1] = parseInt(o[i-1]) / parseInt(o[i+1]);
-                o.splice(i, 2); 
-                i--;
-                break;
-            case "*":
-                o[i-1] = bigIntMultiplication(o[i-1], o[i+1]);
-                o.splice(i, 2) 
-                i--;
-                break;
-        }
+//     if (["*","/"].includes(o[i])){
+//         switch(o[i]) {
+//             case "/":
+//                 o[i-1] = parseInt(o[i-1]) / parseInt(o[i+1]);
+//                 o.splice(i, 2); 
+//                 i--;
+//                 break;
+//             case "*":
+//                 o[i-1] = bigIntMultiplication(o[i-1], o[i+1]);
+//                 o.splice(i, 2) 
+//                 i--;
+//                 break;
+//         }
   
-    }
+//     }
      
-}
-    for(var i = 0 ; i < o.length; i++ ){
+// }
+//     for(var i = 0 ; i < o.length; i++ ){
     
-      if (["+","-"].includes(o[i])){
-          switch(o[i]) {
-              case "-":
-                num1 = o[i-1];
-                num2 = o[i+1];
-                o[i-1] = bigIntSubtraction(num1, num2);  
-                o.splice(i, 2); 
-                i--;
-                break;
-              case "+":
-                num1 = parseInt(o[i-1]);
-                num2 = parseInt(o[i+1]);
-                o[i-1] = bigIntAddition(num1,num2);
-                o.splice(i, 2) 
-                i--;
-                break;
-          }
+//       if (["+","-"].includes(o[i])){
+//           switch(o[i]) {
+//               case "-":
+//                 num1 = o[i-1];
+//                 num2 = o[i+1];
+//                 o[i-1] = bigIntSubtraction(num1, num2);  
+//                 o.splice(i, 2); 
+//                 i--;
+//                 break;
+//               case "+":
+//                 num1 = parseInt(o[i-1]);
+//                 num2 = parseInt(o[i+1]);
+//                 o[i-1] = bigIntAddition(num1,num2);
+//                 o.splice(i, 2) 
+//                 i--;
+//                 break;
+//           }
     
-      }
+//       }
       
     
-  }
+//   }
 
-console.log(o);
-
-
+// console.log(o);
 
 
 
@@ -200,13 +213,100 @@ console.log(o);
 
 
 
-// num1 ='1555';
-// num2 = '1444';
 
 
-// console.log(bigIntMultiplication(num1,num2));
+
+
+function guessQuotient(num2, dividendSection){
+    guess = "1";
+    test = true;
+    while (test == true ) {
+        numberToValidate = bigIntMultiplication(num2,String(guess));
+        if (numberToValidate.localeCompare(dividendSection, undefined, {numeric: true}) == -1 ) {
+            subtraction = bigIntSubtraction(dividendSection,numberToValidate);
+            if (subtraction.localeCompare(num2, undefined, {numeric: true}) == -1) {
+                test =false;
+                break;
+            }
+        }
+        guess++;    
+    }
+    return [guess, subtraction];
+}
+function bigIntDivision(num1, num2) {
+
+    if (num2 == 0) {
+        return "can't divide by 0";
+        
+    }
+
+    if((num1.localeCompare(num2, undefined, {numeric: true}) == -1) || num1 == "0"){
+        return 0;
+    }
+    result = [];
+    let lenDivisor = num2.length;
+    dividendSection = num1.substring(0,lenDivisor);
+    if(dividendSection.localeCompare(num2, undefined, {numeric: true}) == -1){
+        dividendSection = num1.substring(0,lenDivisor+1);
+        num1 = num1.substring(lenDivisor+1);
+    }else{
+        num1 = num1.substring(lenDivisor);
+    }
+
+    test = false;
+    while (num1.length != 0 || !test) {
+        
+        guessResult = guessQuotient(num2, dividendSection);
+        result.push(guessResult[0]);
+        reminder = guessResult[1];
+        // num1 = num1.substring(1);
+        zero = 0;
+        while (reminder.localeCompare(num2, undefined, {numeric: true}) == -1 && num1.length != 0) {
+            if (zero > 0) {
+                result.push(0);
+                // zero = 0;
+            }
+            reminder = reminder + num1[0];
+            num1 = num1.substring(1);
+            zero++;
+        }
+        // if(){
+        //     reminder = reminder + num1[0];
+        //     num1 = num1.substring(1);
+        //     //num1 = num1.substring(lenDivisor+1);
+        // }
+        dividendSection = reminder;
+        test = reminder.localeCompare(num2, undefined, {numeric: true}) == -1;
+
+
+    }
+
+    
+    
+    
+
+    
+    
+
+
+    return result;
+    
+
+    
+}
+
+
+// bigIntDivision(num2,num1);
+num1 = "1232132134353467745645756657";
+num2 = "22222";
+
+console.log(bigIntMultiplication(num1,num2));
 
     
 
 // console.log(res);
+
+
+
+
 
